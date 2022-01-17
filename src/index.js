df52015d3ea61be67f70a0267e731949a2999bad
Thalison Campos <thalison1998@gmail.com>
2022-01-17 18:06:27 -0300

    rota de procura por data criada

--- src/index.js
+++ src/index.js
@@ -1,59 +1,103 @@
 const express = require("express");
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const customers = []
+const customers = [];
 
 const verifiyIfExistsAccountCPF = (request, response, next) => {
-    const { cpf } = request.headers;
+  const { cpf } = request.headers;
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+  const customer = customers.find((customer) => customer.cpf === cpf);
 
-    if(!customer){
-        return response.status(400).json({error:'Customer not found'})
-    }
-    request.customer = customer;
-    return next();
-
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+  request.customer = customer;
+  return next();
 };
 
+const getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  console.log(balance);
+  return balance;
+};
 
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
-app.post('/account',(request,response)=>{
-    const {cpf, name} = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "customer already Eexists" });
+  }
+
+  customers.push({ cpf, name, id: uuidv4(), statement: [] });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", verifiyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifiyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { description, amount } = request.body;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifiyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
   
-    const customerAlreadyExists = 
-    customers.some((customer) => customer.cpf === cpf);
-
-    if(customerAlreadyExists){
-        return response.status(400).json({error:'customer already Eexists'})
-    }
-
-    customers.push({cpf, name, id: uuidv4(), statement:[]});
-
-    return response.status(201).send()
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
 });
 
-app.get('/statement',verifiyIfExistsAccountCPF,(request, response)=>{
-   const {customer} = request;
-   return response.json(customer.statement );
+app.get("/statement/date", verifiyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+  
+  return response.json(statement);
 });
-
-app.post('/deposit',verifiyIfExistsAccountCPF, (request, response) => {
-    const {customer} = request
-    const { description, amount } = request.body;
-
-    const statementOperation = {
-        description,
-        amount,
-        create_at: new Date(),
-        type:"credit"
-    }
-
-    customer.statement.push(statementOperation);
-    return response.status(201).send();
-})
 
 app.listen(3333);
