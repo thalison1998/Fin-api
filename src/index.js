010c13a9c310a40d6752297b28f0aa17cb1aaf18
Thalison Campos <thalison1998@gmail.com>
2022-01-17 16:10:32 -0300

    Criação do middleware de verificação de existencia do cpf

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ app.use(express.json())
 
 const customers = []
 
-/* 
-cpf => string
-name => string
-id => uuid
-statement => []
-*/
+const verifiyIfExistsAccountCPF = (request, response, next) => {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({error:'Customer not found'})
+    }
+    request.customer = customer;
+    return next();
+
+};
+
+
 
 app.post('/account',(request,response)=>{
     const {cpf, name} = request.body;
@@ -28,16 +36,9 @@ app.post('/account',(request,response)=>{
     return response.status(201).send()
 });
 
-app.get('/statement/:cpf',(request, response)=>{
-    const { cpf } = request.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-    console.log(customer)
-    if(!customer){
-        return response.status(400).json({error:'Customer not found'})
-    }
-
-    return response.json(customer.statement );
+app.get('/statement',verifiyIfExistsAccountCPF,(request, response)=>{
+   const {customer} = request;
+   return response.json(customer.statement );
 });
 
 
